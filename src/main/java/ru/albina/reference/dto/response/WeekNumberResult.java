2ae75a38b5eb84db29a537077a59f3461eec06b0
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-08 16:26:52 +0300

    Fix.

--- src/main/java/ru/albina/reference/dto/response/WeekNumberResult.java
+++ src/main/java/ru/albina/reference/dto/response/WeekNumberResult.java
@@ -1,12 +1,16 @@
 package ru.albina.reference.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeekNumberResult {
 
     private LocalDate startDate;
